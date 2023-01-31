d025e9193c4d37e71856eb54913e21b849231794
Luis <fl337789@uaeh.edu.mx>
2023-01-31 09:54:03 -0600

    Creamos el archivo logica.js e inicializamos las variables

---
+++ logica.js
@@ -0,0 +1,27 @@
+var operando1;
+var operando2;
+var operacion;
+
+function init(){
+    var resultado = document.getElementById('resultado');
+    var siete = document.getElementById('7');
+    var ocho = document.getElementById('8');
+    var nueve = document.getElementById('9');
+    var multiplicacion = document.getElementById('multiplicacion');
+    var cuatro = document.getElementById('4');
+    var cinco = document.getElementById('5');
+    var seis = document.getElementById('6');
+    var resta = document.getElementById('resta');
+    var uno = document.getElementById('1');
+    var dos = document.getElementById('2');
+    var tres = document.getElementById('3');
+    var suma = document.getElementById('suma');
+    var resultado = document.getElementById('clear');
+    var cero = document.getElementById('0');
+    var punto = document.getElementById('punto');
+    var igual = document.getElementById('igual');
+
+
+
+
+}
