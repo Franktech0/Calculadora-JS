3dab33954e9e65f30a9c5c432f4ec4f840d9d3de
Luis <fl337789@uaeh.edu.mx>
2023-02-09 17:51:21 -0600

    Se creo el ciclo para realizar las operaciones y la caluladora esta funcionando

--- logica.js
+++ logica.js
@@ -10,6 +10,10 @@ var aux = 0;
 var Arreglo1 = [];
 var PilaOperadores = [];
 var NotPosfijo = [];
+var ArregloReverso = [];
+var SalvarDatos = [];
+var SalvarNumeros = "";
+var AuxDatos = [];
 
 
 function init(){
@@ -119,6 +123,7 @@ function init(){
         //creamos nuestra pila
         //2+2/4  *2-1 = 1
         console.log(cadena.length);
+        console.log(cadena);
         for(let i =0; i<cadena.length; i++){
             
             if((cadena[i]!='+')&&(cadena[i]!='-')&&(cadena[i]!='/')&&(cadena[i]!='*'))
@@ -168,139 +173,93 @@ function init(){
 
         
         
-        console.log(NotPosfijo);
+        console.log(NotPosfijo); //al final nuestro resultado ya esta en notposfijo
         console.log(PilaOperadores);
         console.log(Arreglo1);
         PilaOperadores.forEach(element => {console.log(element)});
         Arreglo1.forEach(element => {console.log(element)});
 
 
+        //ahora solo falta sacar el resultado
+        
+        //ArregloReverso = NotPosfijo.reverse();
+        
 
-        /*
-        //cadena ejemplo 12+12 un arrglo de 5 posiciones contanto el 0
-        console.log(cadena.length)
-        if(cadena.length < 30)
+        //2+2/4 == 224/+
+        //+/422
+        // for(let i = 0; i<NotPosfijo.length; i++)
+        // {
+        //     SalvarNumeros += String(NotPosfijo[i]);
+        // } 
+
+        console.log(SalvarNumeros);
+
+        for(let i = 0; i<NotPosfijo.length; i++)
         {
-        //22+2-2
-            for(var i=0; i<cadena.length; i++)
+            //console.log(NotPosfijo);
+            if((NotPosfijo[i]!='+')&&(NotPosfijo[i]!='-')&&(NotPosfijo[i]!='/')&&(NotPosfijo[i]!='*'))
             {
-                //4+2*6
-                if(cadena[i]!= null)
+                SalvarDatos.push(parseFloat(NotPosfijo[i]));//elimina el primer elemento del arreglo
+                //salvar datos [2, 2, 4 ]
+            }
+            else if(NotPosfijo[i]=='+')
+            {
+                for(let i = 0; i<2; i++)
                 {
-                    if((cadena[i]!='+')&&(cadena[i]!='-')&&(cadena[i]!='/')&&(cadena[i]!='*'))
-                    {
-                        console.log("operando1: " + operando1);
-                        operando1 += cadena[i];
-                        console.log(operando1);
-                    }
-                    if(cadena[i]=='+'){
-                        console.log("opcion suma");
-                        operando2 = operando1; //4
-                        console.log("operando 2:" + operando2);
-                        acumulador += parseFloat(operando2);//
-                        console.log("acumulador: "+acumulador);
-                        operando2 = "";
-                        operando1 = "";
-
-                        opcion = '+';
-                    }
-                    if(cadena[i]=='-')
-                    {
-                        //ya funciona con 2 numeros
-                        console.log("opcion resta");
-                        operando2 = operando1; //operando 2 es igual a 5 //ahora tiene 2
-                        console.log("operando 2: " + operando2);//5 positivo //2 positivo
-                        if((Math.sign(parseFloat(operando2)) == 1) ||(Math.sign(parseFloat(operando2)) == 0))
-                        {
-                            acumulador = parseFloat(operando2);
-                        }
-                        else{
-                            acumulador -= parseFloat(operando2); //5 //2
-                            acumulador *= -1; //-5 //termina con 2 negativo
-                            console.log(acumulador); //imprime -5 //imprime//-2
-                            operando2 = ""; //vacea 
-                            operando1 = ""; //vacea
-                        }
-                        
-                        
-
-                        opcion = "-"; // es la ultima operacion que se realiza antes de romper el ciclo
-                    }
-                    if(cadena[i]=='*')//9*3
-                    {
-                        console.log("opcion multiplicacion");
-                        operando2 = operando1; //9 //3
-                        console.log("operando 2:" + operando2);//9 //3
-                        console.log("acumulador inicial: " + acumulador);
-
-                        if (acumulador == 0)//en la primera vuelta si //aqui el acumulador ya vale 9
-                        {
-                            acumulador = parseFloat(operando2);//9
-                            console.log("entre en la primera condicion " + acumulador);
-                            operando2 = "";
-                            operando1 = "";
-                            
-                        }
-                        else{ //entra en la segunda vuelta
-                            console.log("entre en la segunda condicion");
-                            acumulador *= parseFloat(operando2);// 9/3 = 3
-                            console.log("acumulador: "+acumulador);
-                            operando2 = "";
-                            operando1 = "";
-                        };
-
-                        opcion='*'; // es la ultima operacion que se realiza antes de romper el ciclo
-                    }
-                    if(cadena[i]=='/')
-                    {
-                        console.log("opcion division");
-                        operando2 = operando1; //9 //3
-                        console.log("operando 2:" + operando2);//9 //3
-                        console.log("acumulador inicial: " + acumulador);
-                        
-                        if (acumulador == 0)//en la primera vuelta si //aqui el acumulador ya vale 9
-                        {
-                            acumulador = parseFloat(operando2);//9
-                            console.log("entre en la primera condicion " + acumulador);
-                            opcion='/';
-                            operando2 = "";
-                            operando1 = "";
-                            
-                        }
-                        else{ //entra en la segunda vuelta
-                            console.log("entre en la segunda condicion");
-                            acumulador /= parseFloat(operando2);// 9/3 = 3
-                            console.log("acumulador: "+acumulador);
-                            operando2 = "";
-                            operando1 = "";
-                        };
-
-                        opcion='/'; // es la ultima operacion que se realiza antes de romper el ciclo
-                    }
-
+                    //salvar datos llega con: [2, .5]
+                    AuxDatos[i] = SalvarDatos.pop()//invertimos el orden de los ultimos leementos del arreglo
                 }
-                else
+                //auxdatos = [.5, 2]
+
+                SalvarDatos.push(AuxDatos.pop()+AuxDatos.shift()); 
+                //2+.5 = 2.5
+            }
+            else if(NotPosfijo[i]=='-')
+            {
+                
+                for(let i = 0; i<2; i++)
                 {
-                    alert("Error inesperado, la calculadora no puede realizar esta operacion")
-                    break;
+                    AuxDatos[i] = SalvarDatos.pop()//invertimos el orden de los ultimos leementos del arreglo
                 }
+
+                SalvarDatos.push(AuxDatos.pop()-AuxDatos.shift()); 
+            }
+            else if(NotPosfijo[i]=='*')
+            {
+                
+                for(let i = 0; i<2; i++)
+                {
+                    AuxDatos[i] = SalvarDatos.pop()//invertimos el orden de los ultimos leementos del arreglo
+                }
+
+                SalvarDatos.push(AuxDatos.pop()*AuxDatos.shift()); 
+            }
+            else if(NotPosfijo[i]=='/')
+            {
+                
+                for(let i = 0; i<2; i++)
+                {   
+                    AuxDatos[i] = SalvarDatos.pop()//invertimos el orden de los ultimos leementos del arreglo
+                    //auxdatos = [4, 2]
+                }
+                console.log("este es auxiliar datos en la division: " + AuxDatos);
+
+                SalvarDatos.push(AuxDatos.pop()/AuxDatos.shift());
+                //2/4 = .5
+                //salvardatos[2, .5]
+            }
+            else{
+                alert("¡Hubo un error inesperado!")
             }
         }
-        else{
-            alert("El numero maximo de digitos es 30");
-            resetear();
-        }
-        // operacion = parseFloat(acumulador) + parseFloat(operando1);
-        // operacion = parseFloat(acumulador) - parseFloat(operando1);
-        // console.log(operacion);
-        // resultado.textContent = operacion;
-        console.log(opcion);
-        console.log(operando1);
-        resolver(opcion);
-    
+        console.log(AuxDatos);
+        console.log(SalvarDatos);
+
+        resultado.textContent = String(SalvarDatos);
+
 
     }
-    */
+    
     borrar.onclick = function(){
         resultado.textContent = "";
         console.clear();
@@ -318,35 +277,5 @@ function resetear(){
     Arreglo1 = [];
     PilaOperadores = [];
     NotPosfijo = [];
+    SalvarDatos = [];
 }
-
-function resolver(a){
-    console.log("entre a la funcion resolver");
-    switch (a) {
-        case "+":
-            operacion = parseFloat(acumulador) + parseFloat(operando1);
-            console.log("entre en la opcion sumar");
-            break;
-        case "-":
-            operacion = parseFloat(acumulador) - parseFloat(operando1);
-            console.log("entre en la opcion resta");
-            break;
-        case "*":
-            operacion = parseFloat(acumulador) * parseFloat(operando1);
-            console.log("entre en la opcion multiplicacion");
-            break;
-        case "/":
-            if(acumulador != 0)
-            {
-                operacion = parseFloat(acumulador) / parseFloat(operando1); //acumulador vale 3 y operando 1 vale 2
-                console.log("entre en la opcion division");
-            }
-            else{
-                alert("No se puede dividir un número entre 0");
-            }
-            break;        
-    }
-    resultado.textContent = operacion;
-    
-    //resetear();
-}}
